6106bc2dff34b6e6f621cb31c9ffd72d9e2e6f99
alijameel18 <alijameel2325@gmail.com>
2025-12-16 11:34:30 +0530

    link change

--- src/UserService.js
+++ src/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:8080/api/v1/users";
+const API_BASE_URL = "https://myschool1-production.up.railway.app/api/v1/users";
 
 class UserService {
 
